cfbadb6398a2f85ee99d581e1759f80703fd60ee
hejingkui <hejingkui1@huawei.com>
2023-10-12 10:58:19 +0800

    add save_with_output op

--- backends/npu/custom_op/llama_process_op.cc
+++ backends/npu/custom_op/llama_process_op.cc
@@ -610,30 +610,133 @@ PD_BUILD_OP(top_p_sampling)
         TopPSamplingOpInferShape));  // neccessary if the op has muti_inputs
 
 
-    // save_with_output
-std::vector<paddle::Tensor> SaveWithOutputOp(const paddle::Tensor& batch_idx,
-                                             const paddle::Tensor& step_idx,
-                                             const paddle::Tensor& x) {
-  std::cout<<"SaveWithOutputOp"<<std::endl;
+constexpr char kSEP = '/';
 
-  std::shared_ptr<phi::DenseTensor> out_tensor =
-      std::make_shared<phi::DenseTensor>();
-  
-  return {paddle::Tensor(out_tensor)};
+std::string DirName(const std::string &filepath) {
+  auto pos = filepath.rfind(kSEP);
+  if (pos == std::string::npos) {
+    return "";
+  }
+  return filepath.substr(0, pos);
 }
 
-std::vector<std::vector<int64_t>> SaveWithOutputOpInferShape(
-    const std::vector<int64_t>& batch_idx_shape,
-    const std::vector<int64_t>& step_idx_shape,
-    const std::vector<int64_t>& x_shape) {
-  return {x_shape};
+bool FileExists(const std::string &filepath) {
+  struct stat buffer;
+  return (stat(filepath.c_str(), &buffer) == 0);
 }
 
-// PD_BUILD_OP
+void MkDir(const char *path) {
+  std::string path_error(path);
+  path_error += " mkdir failed!";
+  if (mkdir(path, 0755)) {
+    if (errno != EEXIST) {
+      throw std::runtime_error(path_error);
+    }
+  }
+}
+
+void MkDirRecursively(const char *fullpath) {
+  if (*fullpath == '\0') return;  // empty string
+  if (FileExists(fullpath)) return;
+  MkDirRecursively(DirName(fullpath).c_str());
+  MkDir(fullpath);
+}
+
+
+template<typename data_t>
+void saveToFile(std::ostream & os, const void* x_data, std::vector<int64_t> shape, int64_t x_numel, const char type_id) {
+  // 1.type
+  os.write(reinterpret_cast<const char *>(&type_id),sizeof(type_id));
+  // 2.data
+  uint64_t size = x_numel * sizeof(data_t);
+  os.write(static_cast<const char*>(x_data),static_cast<std::streamsize>(size));
+
+}
+
+template<typename data_t>
+void save_with_output_kernel(const paddle::Tensor& x,
+                             const paddle::Tensor& batch_idx,
+                             const paddle::Tensor& step_idx,
+                             std::string file_path,
+                             int64_t rank_id,
+                             char type_id) {
+  std::vector<int64_t> x_shape = x.shape();
+
+  if(rank_id >= 0) {
+      file_path += "_rank_" + std::to_string(rank_id);
+  }
+
+  int batch_idx_data = -1, step_idx_data = -1;
+
+  if(batch_idx.is_gpu()) {
+    paddle::Tensor batch_idx_cpu = batch_idx.copy_to(paddle::CPUPlace(), 114514);
+    batch_idx_data = batch_idx_cpu.data<int32_t>()[0];
+  } else {
+    batch_idx_data = batch_idx.data<int32_t>()[0];
+  }
+  if(step_idx.is_gpu()) {
+    paddle::Tensor step_idx_cpu = step_idx.copy_to(paddle::CPUPlace(), 114514);
+    step_idx_data = step_idx_cpu.data<int64_t>()[0];
+  } else {
+    step_idx_data = step_idx.data<int64_t>()[0];
+  }
+  auto x_data = x.data<data_t>();
+
+  if(batch_idx_data >= 0) {
+    file_path += "_batch_" + std::to_string(batch_idx_data);
+  }
+  if(step_idx_data >= 0) {
+    file_path += "_step_" + std::to_string(step_idx_data);
+  }
+  MkDirRecursively(DirName(file_path).c_str());
+  std::ofstream fout(file_path, std::ios::binary);
+  fout.write("0",1);
+  saveToFile<data_t>(fout, x_data, x_shape, x.numel(),type_id);
+  fout.seekp(std::ios::beg);
+  fout.write("1",1);
+  fout.close();
+
+}
+
+std::vector<paddle::Tensor> SaveWithOutputForward(const paddle::Tensor& x,
+                                                  const paddle::Tensor& batch_idx,
+                                                  const paddle::Tensor& step_idx,
+                                                  std::string file_path,
+                                                  int64_t rank_id) {
+    auto out = x.copy_to(paddle::CPUPlace(), false);
+    switch(x.type()) {
+      case paddle::DataType::FLOAT32:
+         save_with_output_kernel<float>(out, batch_idx, step_idx, file_path, rank_id, '0');
+         break;
+      case paddle::DataType::INT64:
+        save_with_output_kernel<int64_t>(out, batch_idx, step_idx, file_path, rank_id,'1');
+         break;
+      case paddle::DataType::INT32:
+        save_with_output_kernel<int32_t>(out, batch_idx, step_idx, file_path, rank_id, '2');
+         break;
+      default:
+        PD_THROW("function SaveWithOutputForward is not implemented for data type");
+    }
+   return {out};
+}
+
+std::vector<std::vector<int64_t>> SaveWithOutputInferShape(const std::vector<int64_t>& x_shape,
+                                                           const std::vector<int64_t>& batch_idx_shape,
+                                                           const std::vector<int64_t>& step_idx_shape) {
+    return {x_shape};
+}
+
+std::vector<paddle::DataType> SaveWithOutputInferDtype(const paddle::DataType& x_dtype,
+                                                       const paddle::DataType& batch_idx_dtype,
+                                                       const paddle::DataType& step_idx_dtype) {
+    return {x_dtype};
+}
+
 PD_BUILD_OP(save_with_output)
-    .Inputs({"batch_idx", "step_idx", "x"})
+    .Inputs({"x", "batch_idx", "step_idx"})
+    .Attrs({"file_path: std::string",
+            "rank_id: int64_t"})
     .Outputs({"out"})
-	.Attrs({"file_path: std::string"})
-    .SetKernelFn(PD_KERNEL(SaveWithOutputOp))
-    .SetInferShapeFn(PD_INFER_SHAPE(
-        SaveWithOutputOpInferShape));  // neccessary if the op has muti_inputs
+    .SetKernelFn(PD_KERNEL(SaveWithOutputForward))
+    .SetInferShapeFn(PD_INFER_SHAPE(SaveWithOutputInferShape))
+    .SetInferDtypeFn(PD_INFER_DTYPE(SaveWithOutputInferDtype));
